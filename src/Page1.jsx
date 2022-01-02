a0d02be78571b9cc8d9eab73da0eea84c461b1c7
daiki-tadokoro <81633632+daiki-tadokoro@users.noreply.github.com>
2022-01-02 15:43:23 +0900

    fix:state

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arry = [...Array(100).keys()];
+  console.log(arry);
   return (
     <div>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detailA">Page1DetaikAです</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arry }}>
+        Page1DetaikAです
+      </Link>
       <br />
       <Link to="/page1/detailB">Page1DetaikBです</Link>
     </div>
