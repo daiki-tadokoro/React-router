1a6d15e60a0d07e09ae40fe353b5a78b991f5485
daiki-tadokoro <81633632+daiki-tadokoro@users.noreply.github.com>
2022-01-02 15:56:08 +0900

    fix:history-route

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arry = [...Array(100).keys()];
-  console.log(arry);
+  const history = useHistory();
+  const onClickDetailA = () => history.push("/page1/detailA");
   return (
     <div>
       <h1>Page1ページです</h1>
@@ -11,6 +12,8 @@ export const Page1 = () => {
       </Link>
       <br />
       <Link to="/page1/detailB">Page1DetaikBです</Link>
+      <br />
+      <button onClick={onClickDetailA}>Page1DetaikA</button>
     </div>
   );
 };
