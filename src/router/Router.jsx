4acb2d350b340ccbb3db2def0136d1a7d5261e81
daiki-tadokoro <81633632+daiki-tadokoro@users.noreply.github.com>
2022-01-02 11:13:58 +0900

    fix:route-component

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page1DetailA } from "../Page1DetailA";
-import { Page1DetailB } from "../Page1DetailB";
 import { Page2 } from "../Page2";
+import { Page1Routes } from "./Page1Routes";
 
 export const Router = () => {
   return (
@@ -16,14 +14,19 @@ export const Router = () => {
           path="/page1"
           render={({ match: { url } }) => (
             <Switch>
-              {console.log(url)}
-              <Route exact path={url}>
+              {/* <Route exact path={url}>
                 <Page1 />
               </Route>
               <Route path={`${url}/detailA`} render={() => <Page1DetailA />} />
               <Route path={`${url}/detailB`}>
                 <Page1DetailB />
-              </Route>
+              </Route> */}
+              {/* ループ処理に変更 */}
+              {Page1Routes.map((route) => (
+                <Router key={route.path} exact={route.exact} path=`${url}${route.path}`>
+                  {route.children}
+                </Router>
+              ))}
             </Switch>
           )}
         />
